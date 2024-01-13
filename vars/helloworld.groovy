7d9e2c67c7841e14f0ab7d62ae7caf9435ed81fd
stupidman <hangingfan@gmail.com>
2024-01-13 16:02:37 +0800

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,8 +1,8 @@
 def call(Map config = [:]){
-    record_path ="${config.projectpath}/Build/error_record.txt"
-    if (fileExists ("${config.record_path}")) 
+    record_path ="${config.projectpath}/Build/lta.txt"
+    if (fileExists ("${record_path}")) 
     {
-        error_content = readFile(file: "${config.record_path}")
+        error_content = readFile(file: "${record_path}")
         error_content = error_content.replaceAll("\n", "\\\\n")
         return error_content
     }
