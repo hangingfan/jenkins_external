90f9c9d42bb4c6e8750f75a5bd5165fcf70ef932
stupidman <hangingfan@gmail.com>
2024-01-13 15:57:18 +0800

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,3 +1,10 @@
-def call(){
-    sh "echo ${prebuild_log_path}"
+def call(Map config = [:]){
+    record_path ="${config.projectpath}/Build/error_record.txt"
+    if (fileExists ("${config.record_path}")) 
+    {
+        error_content = readFile(file: "${config.record_path}")
+        error_content = error_content.replaceAll("\n", "\\\\n")
+        return error_content
+    }
+    return ""
 }
