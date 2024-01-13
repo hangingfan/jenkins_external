b4791a0afa540b85c7a6393833afc288bf6d115f
stupidman <hangingfan@gmail.com>
2024-01-13 16:40:55 +0800

    Update CheckOutputFile.groovy

--- vars/CheckOutputFile.groovy
+++ vars/CheckOutputFile.groovy
@@ -1,7 +1,6 @@
 def call(Map config = [:]){
     dir("${config.output_path}")
     {
-        echo "${config.output_path}"
         docFiles = findFiles(glob: "${config.cur_suffix}")
         if (docFiles.size() == 0)
         {
