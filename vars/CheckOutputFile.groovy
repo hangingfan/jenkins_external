660fb94217d192e2ca3f564ac794cdfc1defad2d
stupidman <hangingfan@gmail.com>
2024-01-13 16:12:17 +0800

    Create CheckOutputFile.groovy

---
+++ vars/CheckOutputFile.groovy
@@ -0,0 +1,11 @@
+def call(Map config = [:]){
+    dir("${config.output_path")
+    {
+        docFiles = findFiles(glob: "${config.cur_suffix}")
+        if (docFiles.size() == 0)
+        {
+            uploadAfterFailed()
+            error("${config.cur_suffix} not found, aborted the jobs")
+        }
+    }
+}
