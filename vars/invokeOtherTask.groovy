4cae699f65466246b07597632e579ed13b263287
stupidman <hangingfan@gmail.com>
2024-01-27 16:59:27 +0800

    Create invokeOtherTask.groovy

---
+++ vars/invokeOtherTask.groovy
@@ -0,0 +1,12 @@
+def call(Map config = [:]){
+    build job: currentJobName, wait: false, parameters: [
+        string(name: 'channel', value: "${config.channel}"),
+        string(name: 'buildtype', value: "${config.buildtype}"),
+        string(name: 'auto_trigger', value: "${config.auto_trigger}"),
+        string(name: 'sdk_env', value: "${config.sdk_env}"),
+        string(name: 'u8_enable', value: "${config.u8_enable}"),
+        string(name: 'version_env', value: "${config.version_env}"),
+        string(name: 'regenerate', value: "${config.regenerate}"),
+        string(name: 'origin_package', value: "${config.origin_package}"),
+        ]
+}
