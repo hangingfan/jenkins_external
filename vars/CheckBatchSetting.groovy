7e197c40d634e0fc2a2018a4cd85d25633554a02
stupidman <hangingfan@gmail.com>
2024-01-27 17:20:31 +0800

    Update CheckBatchSetting.groovy

--- vars/CheckBatchSetting.groovy
+++ vars/CheckBatchSetting.groovy
@@ -1,13 +1,13 @@
 def call(Map config = [:]){
     if("${config.channel}".contains('批量官包_withboard'))
     {
-        ${config.channel} = 'official_android'
-        ${config.buildtype} = 'release_withboard'
-        ${config.auto_trigger} = 'None'
-        ${config.download_latest} = '1'
-        ${config.sdk_env} = 'prod'
-        ${config.regenerate} = '1'
-        ${config.u8_enable} = '1'
+        config.channel = 'official_android'
+        config.buildtype = 'release_withboard'
+        config.auto_trigger = 'None'
+        config.download_latest = '1'
+        config.sdk_env = 'prod'
+        config.regenerate = '1'
+        config.u8_enable = '1'
 
         invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'bilibili', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
         invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'huawei', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
@@ -18,13 +18,13 @@ def call(Map config = [:]){
     }
     else if("${config.channel}".contains('批量官包release'))
     {
-        ${config.channel} = 'official_android'
-        ${config.buildtype} = 'release'
-        ${config.auto_trigger} = 'None'
-        ${config.download_latest} = '1'
-        ${config.sdk_env} = 'prod'
-        ${config.regenerate} = '1'
-        ${config.u8_enable} = '1'
+        config.channel = 'official_android'
+        config.buildtype = 'release'
+        config.auto_trigger = 'None'
+        config.download_latest = '1'
+        config.sdk_env = 'prod'
+        config.regenerate = '1'
+        config.u8_enable = '1'
 
         invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'bilibili', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
         invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'huawei', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
