5c879060a72f9457252e0afa75b59627151c0dd4
stupidman <hangingfan@gmail.com>
2024-02-26 15:52:06 +0800

    Update crashsight_upload.groovy

--- vars/crashsight_upload.groovy
+++ vars/crashsight_upload.groovy
@@ -12,4 +12,18 @@ def call(Map config = [:]){
         crash_app_id_ios = 'b3f332fe29'
         crash_app_key_ios = '99e1fdc8-a626-4272-83fb-49ea074c794c'
     }
+
+    dir("${config.projectpath}"/Build/tools/crashsight)
+    {
+        crashsight_command_ios = "java -jar crashSightSymbolTool.jar -id ${crash_app_id_ios} -key ${crash_app_key_ios} -package ${config.bundleid} -version ${config.package_v} -p ios -i ${config.projectpath}/Build/xcarchives/latest.xcarchive/dSYMs -d -u -channel ${config.channel} -url ${crash_upload_url}"
+        crashsight_command_an = "java -jar crashSightSymbolTool.jar -id ${crash_app_id_an} -key ${crash_app_key_an} -package ${config.bundleid} -version ${config.package_v} -buildNo ${config.build_num} -p aos -i ${config.bugly_android_path} -d -u -channel ${config.channel} -url ${crash_upload_url}"
+        if("${config.target}".contains('ios'))
+        {
+            sh(script: "${crashsight_command_ios}")
+        }
+        else
+        {
+            sh(script: "${crashsight_command_an}")
+        }
+    }
 }
