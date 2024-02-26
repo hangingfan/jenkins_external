8a9283f658c27c8b29d0719ad948361447da2666
stupidman <hangingfan@gmail.com>
2024-02-26 15:37:52 +0800

    Update crashsight_upload.groovy

--- vars/crashsight_upload.groovy
+++ vars/crashsight_upload.groovy
@@ -4,7 +4,7 @@ def call(Map config = [:]){
     crash_app_key_an = '868fc7d8-8891-4e9b-9f49-2435e842e7e1'
     crash_app_id_ios = '8d186edcb7'
     crash_app_key_ios = 'f65ae4e4-cefc-41e3-81de-285df2658ca1'
-    if($config.channel}.contains('apple_outside') || ${config.channel}.contains('googleplay'))
+    if("$config.channel}".contains('apple_outside') || "${config.channel}".contains('googleplay'))
     {
         crash_upload_url = 'https://crashsight.wetest.net/openapi/file/upload/symbol'
         crash_app_id_an = 'e2c09c9257'
@@ -13,7 +13,7 @@ def call(Map config = [:]){
         crash_app_key_ios = '99e1fdc8-a626-4272-83fb-49ea074c794c'
     }
 
-    dir("${config.projectpath}/Build/tools/crashsight")
+    dir("${config.projectpath}"/Build/tools/crashsight")
     {
         crashsight_command_ios = "java -jar crashSightSymbolTool.jar -id ${crash_app_id_ios} -key ${crash_app_key_ios} -package ${config.bundleid} -version ${config.package_v} -p ios -i ${config.projectpath}/Build/xcarchives/latest.xcarchive/dSYMs -d -u -channel ${config.channel} -url ${crash_upload_url}"
         crashsight_command_an = "java -jar crashSightSymbolTool.jar -id ${crash_app_id_an} -key ${crash_app_key_an} -package ${config.bundleid} -version ${config.package_v} -buildNo ${config.build_num} -p aos -i ${config.bugly_android_path} -d -u -channel ${config.channel} -url ${crash_upload_url}"
