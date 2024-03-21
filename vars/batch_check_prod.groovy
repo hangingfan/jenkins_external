7fda3949875679c9a1ae4de0d0f05bf407f2648c
stupidman <hangingfan@gmail.com>
2024-03-21 16:10:49 +0800

    Update batch_check_prod.groovy

--- vars/batch_check_prod.groovy
+++ vars/batch_check_prod.groovy
@@ -52,40 +52,36 @@ def call(Map config = [:]){
     }
     else if("${config.channel}".contains('批量official_ios'))
     {
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'release', regenerate:'1', origin_package:'0')
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preofficial_ios', buildtype:'release', regenerate:'0', origin_package:'1')
-
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'debug', regenerate:'0', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'1', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preofficial_ios', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'debug', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'quicktest', origin_package:'0')
 
         currentBuild.result = 'ABORTED'
         error('Manually aborted due to 批量任务')
     }
     else if("${config.channel}".contains('批量official_ios_withboard'))
     {
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'release_withboard', regenerate:'1', origin_package:'0')
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preofficial_ios', buildtype:'release_withboard', regenerate:'0', origin_package:'1')
-
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'debug', regenerate:'0', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'1', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preofficial_ios', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'official_ios', buildtype:'debug', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'quicktest', origin_package:'0')
 
         currentBuild.result = 'ABORTED'
         error('Manually aborted due to 批量任务')
     }
     else if("${config.channel}".contains('批量apple_outside'))
     {
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'批量apple_outside', buildtype:'release', regenerate:'1', origin_package:'0')
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'pre批量apple_outside', buildtype:'release', regenerate:'0', origin_package:'1')
-
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'批量apple_outside', buildtype:'debug', regenerate:'0', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'apple_outside', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'1', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preapple_outside', buildtype:'release', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'apple_outside', buildtype:'debug', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'quicktest', origin_package:'0')
 
         currentBuild.result = 'ABORTED'
         error('Manually aborted due to 批量任务')
     }
     else if("${config.channel}".contains('批量apple_outside_withboard'))
     {
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'批量apple_outside', buildtype:'release_withboard', regenerate:'1', origin_package:'0')
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'pre批量apple_outside', buildtype:'release_withboard', regenerate:'0', origin_package:'1')
-
-        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'批量apple_outside', buildtype:'debug', regenerate:'0', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'apple_outside', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'1', origin_package:'0')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'preapple_outside', buildtype:'release_withboard', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'0', origin_package:'1')
+        invokeOtherTask(currentJobName:"${config.currentJobName}", channel:'apple_outside', buildtype:'debug', auto_trigger:'None', sdk_env:'prod', u8_enable:'1', version_env:"${config.version_env}", regenerate:'quicktest', origin_package:'0')
 
         currentBuild.result = 'ABORTED'
         error('Manually aborted due to 批量任务')
