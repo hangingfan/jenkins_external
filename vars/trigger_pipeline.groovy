91f2552de82a7610f7313b0c0d1c7f8b126ba808
stupidman <hangingfan@gmail.com>
2024-03-25 11:49:56 +0800

    Update trigger_pipeline.groovy

--- vars/trigger_pipeline.groovy
+++ vars/trigger_pipeline.groovy
@@ -1,22 +1,22 @@
 def call(Map config = [:]){
     enable_perf_type = '0'
-    url = "https://tech-jenkins.int.hypergryph.com"
     if("${config.auto_trigger}".contains('PhoneProfile'))
     {
         enable_perf_type = '1'
     }
 
-    label = "ui_auto"
-    teardownCases = "tc_stop_game"
-    setupCases = "tc_install_game,tc_start_game"
+    serial = "R5CR11VKZ8W"
+    platform = "android"
+    token = "5a0Pd1KoLiFUDUaS"
+
     if("${config.target}".contains('ios'))
     {
-        udid = "00008110-0008389C0E01401E"
-        sh "curl -s -u dm02_bm:DM02_BM -X POST ${url}/view/DM02%20\\(Mainline\\)/job/UI_AUTOMATION_IOS_Mainline/buildWithParameters -d label=${label} -d packagePath=${config.triggerPackagePath} -d udid=${udid} -d setupCases=${setupCases} -d testCases=${config.testCases} -d teardownCases=${teardownCases} -d taskID=16 -d classifyPerf=${config.classifyPerf} -d bundleid=${config.bundleid} -d packageVersion=${config.curfileName} -d enablePerfType=${enable_perf_type}"
+        serial = "00008110-0008389C0E01401E"
+        platform = "ios"
     }
-    else
+
+    dir("${projectpath}/Build/tools/autopipeline")
     {
-        udid = "R5CR11VKZ8W"
-        sh "curl -s -u dm02_bm:DM02_BM -X POST ${url}/view/DM02%20\\(Mainline\\)/job/UI_AUTOMATION_AND_Mainline/buildWithParameters -d label=${label} -d packagePath=${config.triggerPackagePath} -d serial=${udid} -d setupCases=${setupCases} -d testCases=${config.testCases} -d teardownCases=${teardownCases} -d taskID=16 -d classifyPerf=${config.classifyPerf} -d bundleid=${config.bundleid} -d packageVersion=${config.curfileName} -d enablePerfType=${enable_perf_type}"
+        sh(script: """python3 run.py --package_path ${config.triggerPackagePath} --serial ${serial} --classify_perf ${config.classifyPerf} --platform ${platform} --package_version ${config.curfileName}  --enable_perf_type ${enable_perf_type} --bundle_id ${config.bundleid} --token ${token}""", returnStdout: true).trim()
     }
 }
